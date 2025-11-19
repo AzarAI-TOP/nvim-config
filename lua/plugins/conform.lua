7b73cf45fccc24a3cc8a268c144a26f63ea48fde
AzarAI <azaraitop@gmail.com>
2025-11-19 15:25:30 +0800

    feat: add conform.nvim plugin for code formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,11 +2,14 @@
 
 ---@type LazySpec
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      python = { "isort", "black" },
-      lua = { "stylua" },
+    "stevearc/conform.nvim",
+    opts = {
+        formatters_by_ft = {
+            python = { "isort", "black" },
+            lua = { "stylua" },
+        },
+
+        lsp_format = "fallback",
+        timeout_ms = 1000,
     },
-  },
 }
