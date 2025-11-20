60a96da4cce038a65248aacd67ebc7c11a054779
AzarAI <azaraitop@gmail.com>
2025-11-20 09:51:53 +0800

    feat: add luv third party library support with formatting file

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -3,30 +3,37 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = {
-    ".luarc.json",
-    ".luarc.jsonc",
-    ".luacheckrc",
-    ".stylua.toml",
-    "stylua.toml",
-    "selene.toml",
-    "selene.yml",
-    ".git",
-  },
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-      },
-      signatureHelp = { enabled = true },
-      workspace = {
-        library = {
-          vim.env.VIMRUNTIME,
-          "$XDG_DATA_HOME/nvim/lazy"
-        },
-      },
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_markers = {
+        ".luarc.json",
+        ".luarc.jsonc",
+        ".luacheckrc",
+        ".stylua.toml",
+        "stylua.toml",
+        "selene.toml",
+        "selene.yml",
+        ".git",
     },
-  },
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT",
+            },
+            signatureHelp = { enabled = true },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    "$XDG_DATA_HOME/nvim/lazy",
+                    -- Depending on the usage, you might want to add additional paths here.
+                    "${3rd}/luv/library",
+                    -- "${3rd}/busted/library",
+                },
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                -- library = vim.api.nvim_get_runtime_file("", true)
+            },
+        },
+    },
 }
